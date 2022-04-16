d2dab5a53879b594b62c3081806fbb3372b63ef1
Brandon Oh <brandonoh2938@gmail.com>
2022-04-15 23:01:54 -0400

    Created movement and environment

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed = 5f;
+    private Rigidbody rigb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigb.AddForce(movement * speed);               
     }
 }
